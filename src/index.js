ea81e2ba1ecbe24ee0c8c60dfc2fec76b07dfc14
arnav <arnavkulkarni127@gmail.com>
2026-06-11 08:29:20 +0530

    style sheet added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import homeLoader from "./home.js"
 import menuLoader from "./menu.js";
 import contactLoader from "./contact.js";
+import "./style.css";
 homeLoader();
 const contact = document.getElementById("contactBtn");
 const menuBtn = document.getElementById("menuBtn");
